88b8ad91cd57bc3eb772efad97f7c444f851c06f
AakashCode12 <aakashcoder1210@gmail.com>
2020-09-11 02:33:43 +0530

    the kick and tom done

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,29 @@ var chanchan2kiawaz = new Audio("../Sounds/909-Crash-HD0.wav");
 function chanchan2() {
     chanchan2kiawaz.play();
 }
+
+
+var kick1kiawaz = new Audio("../Sounds/909-Kick-T0A0D0.wav");
+
+function kick1() {
+    kick1kiawaz.play();
+}
+
+var kick2kiawaz = new Audio("../Sounds/909-Kick-T0A0D3.wav");
+
+function kick2() {
+    kick2kiawaz.play();
+}
+
+
+var tom1kiawz = new Audio("../Sounds/909-HiHatClosed-D8.wav");
+
+function tom1() {
+    tom1kiawz.play();
+}
+
+var tom2kiawz = new Audio("../Sounds/909-HiTom-0D3.wav");
+
+function tom2() {
+    tom2kiawz.play();
+}
