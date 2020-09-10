a2640c83062babc0ad2be232c4fd9898afe96331
AakashCode12 <aakashcoder1210@gmail.com>
2020-09-11 02:22:42 +0530

    chanchan done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-console.log("the drum is awesome");
+var chanchan1kiawaz = new Audio("../Sounds/909-Crash-HD2.wav");
+
+function chanchan1() {
+    chanchan1kiawaz.play();
+}
+
+
+var chanchan2kiawaz = new Audio("../Sounds/909-Crash-HD0.wav");
+
+function chanchan2() {
+    chanchan2kiawaz.play();
+}
